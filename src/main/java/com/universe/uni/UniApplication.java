a9da18124981dc2da8444b52091c5fa20619d5c9
jinsu4755 <parkjinsu4755@gmail.com>
2023-06-21 03:21:03 +0900

    feat: Project Initialization

---
+++ src/main/java/com/universe/uni/UniApplication.java
@@ -0,0 +1,13 @@
+package com.universe.uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/universe/uni/UniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universe.uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
