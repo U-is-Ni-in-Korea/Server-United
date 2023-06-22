ff678597db24f5575e91531f7b48abc84e065b8c
jinsu4755 <parkjinsu4755@gmail.com>
2023-06-22 18:18:43 +0900

    style: Fix code style

--- src/main/java/com/universe/uni/UniApplication.java
+++ src/main/java/com/universe/uni/UniApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UniApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UniApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UniApplication.class, args);
+	}
 
 }
