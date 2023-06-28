4cccfc2abf4e1c99282e3e32e6afaa321fdc2e51
이빵주 <yungju9982@naver.com>
2023-06-28 20:31:47 +0900

    feat: Implement ErrorType

---
+++ src/main/java/com/universe/uni/common/dto/ErrorType.java
@@ -0,0 +1,42 @@
+package com.universe.uni.common.dto;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorType {
+
+	/**
+	 * 400 BAD REQUEST
+	 */
+	VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."), VALIDATION_REQUEST_MISSING_EXCEPTION(
+		HttpStatus.BAD_REQUEST, "요청값이 입력되지 않았습니다."),
+
+	/**
+	 * 404 NOT FOUND
+	 */
+	NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다"),
+
+	/**
+	 * 409 CONFLICT
+	 */
+	ALREADY_EXIST_USER_EXCEPTION(HttpStatus.CONFLICT, "이미 존재하는 유저입니다"),
+
+	/**
+	 * 500 INTERNAL SERVER ERROR
+	 */
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 서버 에러가 발생하였습니다.");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	public int getHttpStatusCode() {
+		return httpStatus.value();
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
