4b785b871de168ee2a3d1129f756f5a83fec7c0e
jiyeon <sjy6736@naver.com>
2023-07-06 03:29:32 +0900

    feat: ShortMatch 수정

--- src/main/java/com/universe/uni/domain/entity/ShortMatch.java
+++ src/main/java/com/universe/uni/domain/entity/ShortMatch.java
@@ -5,14 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorValue("ShortMatch")
 @PrimaryKeyJoinColumn(name = "match_id")
 public class ShortMatch extends Match {
