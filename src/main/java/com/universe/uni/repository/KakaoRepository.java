ec3a8d0facd0fd2e5572c8cc7831c20bdd9af316
jinsu4755 <parkjinsu4755@gmail.com>
2023-07-11 08:41:02 +0900

    feat: Add KakaoAuth fetchToToken Authorized code

--- src/main/java/com/universe/uni/repository/KakaoRepository.java
+++ src/main/java/com/universe/uni/repository/KakaoRepository.java
@@ -1,4 +1,7 @@
 package com.universe.uni.repository;
 
+import com.universe.uni.external.response.KakaoAuthResponse;
+
 public interface KakaoRepository {
+	KakaoAuthResponse fetchTokenBy(String code);
 }
--- src/main/java/com/universe/uni/repository/RemoteKakaoRepository.java
+++ src/main/java/com/universe/uni/repository/RemoteKakaoRepository.java
@@ -3,6 +3,9 @@ package com.universe.uni.repository;
 import org.springframework.stereotype.Repository;
 
 import com.universe.uni.config.AuthConfig;
+import com.universe.uni.external.KakaoAuthClient;
+import com.universe.uni.external.request.KakaoAuthRequest;
+import com.universe.uni.external.response.KakaoAuthResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,5 +13,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RemoteKakaoRepository implements KakaoRepository {
 	private final AuthConfig authConfig;
+	private final KakaoAuthClient kakaoAuthClient;
 
+	@Override
+	public KakaoAuthResponse fetchTokenBy(String code) {
+		final KakaoAuthRequest request = KakaoAuthRequest.builder()
+			.code(code)
+			.grantType("authorization_code")
+			.clientId(authConfig.getKakaoClientId())
+			.clientSecret(authConfig.getKakaoClientSecret())
+			.redirectUri(authConfig.getKakaoRedirectAuth())
+			.build();
+		return kakaoAuthClient.requestAuthToken(request);
+	}
 }
