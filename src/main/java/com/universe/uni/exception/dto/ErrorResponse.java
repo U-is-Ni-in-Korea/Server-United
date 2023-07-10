f4393c4b5c594d961b69ea72a4399e970187b3a4
jinsu4755 <parkjinsu4755@gmail.com>
2023-07-10 16:54:55 +0900

    feat: MVC exception strategy
    기존 에러 처리 방식에서 String이 처리하는 방식이 아닌 경우 예외가 처리되지 않아 수정함.
    
    이외 토큰 부분 또한 Business 관련 Exception 을 사용하도록 변경

--- src/main/java/com/universe/uni/exception/dto/ErrorResponse.java
+++ src/main/java/com/universe/uni/exception/dto/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.universe.uni.exception.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -11,9 +12,14 @@ import lombok.RequiredArgsConstructor;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
 
+	@JsonProperty("code")
 	private final String uniErrorCode;
 
-	public static ErrorResponse error(ErrorType error) {
+	public static ErrorResponse businessErrorOf(ErrorType error) {
 		return new ErrorResponse(error.getUniErrorCode());
+	}
+
+	public static ErrorResponse errorOf(int errorCode) {
+		return new ErrorResponse("UE0" + errorCode);
 	}
 }
--- src/main/java/com/universe/uni/exception/dto/ErrorType.java
+++ src/main/java/com/universe/uni/exception/dto/ErrorType.java
@@ -1,5 +1,6 @@
 package com.universe.uni.exception.dto;
 
+import java.util.Arrays;
 import org.springframework.http.HttpStatus;
 
 import lombok.AccessLevel;
@@ -11,26 +12,49 @@ public enum ErrorType {
 	/**
 	 * 400 BAD REQUEST
 	 */
-	VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "", "잘못된 요청입니다."),
-	VALIDATION_REQUEST_MISSING_EXCEPTION(HttpStatus.BAD_REQUEST, "", "요청값이 입력되지 않았습니다."),
+	VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "UE1001", "잘못된 요청입니다."),
+	VALIDATION_REQUEST_MISSING_EXCEPTION(HttpStatus.BAD_REQUEST, "UE1002", "요청값이 입력되지 않았습니다."),
+
+	/**
+	 * 401 UNAUTHORIZED
+	 */
+	EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "UE2001", "만료된 토큰 입니다."),
+	UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "UE2002", "잘못된 토큰 입니다"),
 
 	/**
 	 * 404 NOT FOUND
 	 */
-	NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "", "존재하지 않는 유저입니다"),
+	METHOD_NOT_SUPPORTED(HttpStatus.NOT_FOUND, "UE5001", "존제하지 않는 EndPoint 입니다"),
+	NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "UE5002", "존재하지 않는 유저입니다"),
+
+	/**
+	 * 406 Not Acceptable
+	 */
+	NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "UE7001", "Accept 헤더에 유효하지 않거나 지원되지 않는 미디어 유형을 지정되었습니다."),
+
 
 	/**
 	 * 409 CONFLICT
 	 */
-	ALREADY_EXIST_USER_EXCEPTION(HttpStatus.CONFLICT, "", "이미 존재하는 유저입니다"),
+	ALREADY_EXIST_USER_EXCEPTION(HttpStatus.CONFLICT, "UE10001", "이미 존재하는 유저입니다"),
+
+	/**
+	 * 415 Unsupported Media Type.
+	 */
+	UNSUPPORTED_METHOD_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "UE16001", "지원하지 않는 미디어 포멧입니다."),
 
 	/**
 	 * 500 INTERNAL SERVER ERROR
 	 */
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "", "예상치 못한 서버 에러가 발생하였습니다.");
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "UE0500", "예상치 못한 서버 에러가 발생하였습니다."),
+
+	/**
+	 * 503 Service Unavailable
+	 */
+	SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "UE0503", "서버가 작동중이지 않거나 과부하 상태입니다.");
 
 	private final HttpStatus httpStatus;
-	private String uniErrorCode;
+	private final String uniErrorCode;
 	private final String message;
 
 	public int getHttpStatusCode() {
@@ -43,5 +67,15 @@ public enum ErrorType {
 
 	public String getMessage() {
 		return message;
+	}
+
+	private boolean hasErrorType(HttpStatus httpStatus) {
+		return this.httpStatus.value() == httpStatus.value();
+	}
+
+	public static ErrorType findErrorTypeBy(HttpStatus httpStatus) {
+		return Arrays.stream(values()).filter((errorType -> errorType.hasErrorType(httpStatus)))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException("UnSupported Business HttpStatus Code :" + httpStatus));
 	}
 }
