a9eaeeb3c2fa9a3fe0211581b5fc18af64c80fa2
이빵주 <yungju9982@naver.com>
2023-07-11 15:27:00 +0900

    feat: Fix to record

--- src/main/java/com/universe/uni/dto/response/UpdateWishCouponResponseDto.java
+++ src/main/java/com/universe/uni/dto/response/UpdateWishCouponResponseDto.java
@@ -1,41 +1,13 @@
 package com.universe.uni.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class UpdateWishCouponResponseDto {
-	private Long id;
-	private String image;
-	private String content;
-	@JsonProperty("isVisible")
-	private boolean visible;
-	@JsonProperty("isUsed")
-	private boolean used;
-	private String usedAt;
-	private String gameType;
-
-	@Builder
-	public UpdateWishCouponResponseDto(
-		Long id,
-		String image,
-		String content,
-		boolean visible,
-		boolean used,
-		String usedAt,
-		String gameType
-	) {
-		this.id = id;
-		this.image = image;
-		this.content = content;
-		this.visible = visible;
-		this.used = used;
-		this.usedAt = usedAt;
-		this.gameType = gameType;
-	}
+@JsonPropertyOrder({"id", "image", "content", "isVisible", "isUsed", "usedAt", "gameType"})
+@Builder
+public record UpdateWishCouponResponseDto(Long id, String image, String content,
+										  @JsonProperty("isVisible") boolean visible,
+										  @JsonProperty("isUsed") boolean used, String usedAt, String gameType) {
 }
