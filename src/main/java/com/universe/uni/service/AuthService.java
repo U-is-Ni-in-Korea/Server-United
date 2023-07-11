c8dbbd2e39eb7b0ecad755a86cf27d6ed0cf2aac
jinsu4755 <parkjinsu4755@gmail.com>
2023-07-11 09:26:30 +0900

    feat: Create authWithKakao method

--- src/main/java/com/universe/uni/service/AuthService.java
+++ src/main/java/com/universe/uni/service/AuthService.java
@@ -1,5 +1,20 @@
 package com.universe.uni.service;
 
+import org.springframework.stereotype.Service;
+
+import com.universe.uni.dto.AuthTokenDto;
+import com.universe.uni.repository.KakaoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService implements AuthServiceContract {
 
+	private final JwtManager jwtManager;
+	private final KakaoRepository kakaoRepository;
+
+	public AuthTokenDto authWithKakao(String code) {
+
+	}
 }
