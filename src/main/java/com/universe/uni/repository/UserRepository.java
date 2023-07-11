bc0f80b230dd3297d826670af47f092d00b9ce0c
jinsu4755 <parkjinsu4755@gmail.com>
2023-07-11 09:38:50 +0900

    feat: Add UserRepository findBySnsAuthCode

--- src/main/java/com/universe/uni/repository/UserRepository.java
+++ src/main/java/com/universe/uni/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.universe.uni.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.universe.uni.domain.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findBySnsAuthCode(String snsAuthCode);
 }
