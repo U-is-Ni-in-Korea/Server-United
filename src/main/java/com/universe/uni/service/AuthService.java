de826cb546156f6ea3edf57c96ba9cde10242252
jinsu4755 <parkjinsu4755@gmail.com>
2023-07-11 09:39:54 +0900

    feat: Add authWithKakao logic
    카카오 서비스를 통해 로그인하는 유저를 저장하기 위한 로직 작성

--- src/main/java/com/universe/uni/service/AuthService.java
+++ src/main/java/com/universe/uni/service/AuthService.java
@@ -1,9 +1,16 @@
 package com.universe.uni.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.universe.uni.domain.SnsType;
+import com.universe.uni.domain.entity.User;
 import com.universe.uni.dto.AuthTokenDto;
+import com.universe.uni.external.response.KakaoAuthResponse;
+import com.universe.uni.external.response.KakaoUserResponse;
 import com.universe.uni.repository.KakaoRepository;
+import com.universe.uni.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,8 +20,26 @@ public class AuthService implements AuthServiceContract {
 
 	private final JwtManager jwtManager;
 	private final KakaoRepository kakaoRepository;
+	private final UserRepository userRepository;
 
+	@Override
+	@Transactional
 	public AuthTokenDto authWithKakao(String code) {
+		final KakaoAuthResponse kakaoToken = kakaoRepository.fetchTokenBy(code);
+		final KakaoUserResponse kakaoUser = kakaoRepository.getUser(kakaoToken.accessToken());
+		final User user = userRepository.findBySnsAuthCode(kakaoUser.id().toString())
+			.orElseGet(() -> userRepository.save(registerKakaoUser(kakaoUser)));
+		return beIssuedAuthToken(user.getId());
+	}
 
+	private User registerKakaoUser(KakaoUserResponse kakaoUser) {
+		return User.builder()
+			.snsType(SnsType.KAKAO)
+			.snsAuthCode(kakaoUser.id().toString())
+			.build();
+	}
+
+	private AuthTokenDto beIssuedAuthToken(long userId) {
+		return jwtManager.issueToken(userId);
 	}
 }
--- src/main/java/com/universe/uni/service/AuthServiceContract.java
+++ src/main/java/com/universe/uni/service/AuthServiceContract.java
@@ -2,5 +2,9 @@ package com.universe.uni.service;
 
 import javax.transaction.Transactional;
 
+import com.universe.uni.dto.AuthTokenDto;
+
 public interface AuthServiceContract {
+	@Transactional
+	AuthTokenDto authWithKakao(String code);
 }
