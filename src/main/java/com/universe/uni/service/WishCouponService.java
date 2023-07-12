652612f97e9a6c0e9790834e107527ce1ccedb33
YeongJu Lee <84129098+2zerozu@users.noreply.github.com>
2023-07-12 14:26:42 +0900

    #27 [feat] 소원권 상세보기 Api 구현 (#28)
    * feat: Add WishCouponResponseDto
    
    * feat: Add JsonProperty
    
    * chore: Change function name
    
    * feat: WishCoupon Get Api

--- src/main/java/com/universe/uni/service/WishCouponService.java
+++ src/main/java/com/universe/uni/service/WishCouponService.java
@@ -9,7 +9,9 @@ import com.universe.uni.domain.GameType;
 import com.universe.uni.domain.entity.WishCoupon;
 import com.universe.uni.dto.request.UpdateWishCouponRequestDto;
 import com.universe.uni.dto.response.UpdateWishCouponResponseDto;
+import com.universe.uni.dto.response.WishCouponResponseDto;
 import com.universe.uni.exception.BadRequestException;
+import com.universe.uni.exception.NotFoundException;
 import com.universe.uni.exception.dto.ErrorType;
 import com.universe.uni.repository.WishCouponRepository;
 
@@ -37,14 +39,38 @@ public class WishCouponService {
 		wishCoupon.updateContent(requestDto.content());
 		wishCoupon.makeVisible();
 
-		return fromWishCoupon(wishCoupon);
+		return fromWishCouponToUpdateWishCouponResponseDto(wishCoupon);
 	}
 
-	private UpdateWishCouponResponseDto fromWishCoupon(WishCoupon wishCoupon) {
+	@Transactional
+	public WishCouponResponseDto getWishCoupon(Long wishCouponId) {
+		WishCoupon wishCoupon = wishCouponRepository.findById(wishCouponId)
+			.orElseThrow(() -> new NotFoundException(ErrorType.INVALID_ENDPOINT_EXCEPTION));
+		return fromWishCouponToWishCouponResponseDto(wishCoupon);
+	}
+
+	private UpdateWishCouponResponseDto fromWishCouponToUpdateWishCouponResponseDto(WishCoupon wishCoupon) {
 		String usedAt = wishCoupon.getUsedAt() != null ? wishCoupon.getUsedAt().toString() : null;
 
 		return UpdateWishCouponResponseDto.builder()
 			.id(wishCoupon.getId())
+			.image(wishCoupon.getImage())
+			.content(wishCoupon.getContent())
+			.visible(wishCoupon.isVisible())
+			.used(wishCoupon.isUsed())
+			.usedAt(usedAt)
+			.gameType(String.valueOf(wishCoupon.getGameType()))
+			.build();
+	}
+
+	private WishCouponResponseDto fromWishCouponToWishCouponResponseDto(WishCoupon wishCoupon) {
+		/** TODO 영주 : 추후 1L 내 userId로 바꾸기*/
+		boolean isMine = wishCoupon.getUser().getId() == 1L;
+		String usedAt = wishCoupon.getUsedAt() != null ? wishCoupon.getUsedAt().toString() : null;
+
+		return WishCouponResponseDto.builder()
+			.id(wishCoupon.getId())
+			.isMine(isMine)
 			.image(wishCoupon.getImage())
 			.content(wishCoupon.getContent())
 			.visible(wishCoupon.isVisible())
