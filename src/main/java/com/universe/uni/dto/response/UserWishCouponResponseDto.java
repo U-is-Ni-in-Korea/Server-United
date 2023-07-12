06c2ef27aab30eb913f7e39f85f2b9be1bcfd3f2
YeongJu Lee <84129098+2zerozu@users.noreply.github.com>
2023-07-13 04:36:37 +0900

    #29 [feat] 유저 소원권 전체 조회하기 Api 구현 (#41)
    * feat: Add UserWishCouponResponseDto
    
    * feat: UserWishCouponList Api

---
+++ src/main/java/com/universe/uni/dto/response/UserWishCouponResponseDto.java
@@ -0,0 +1,16 @@
+package com.universe.uni.dto.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import lombok.Builder;
+
+@JsonPropertyOrder({"availableWishCoupon", "newWishCoupon", "wishCouponList"})
+@Builder
+public record UserWishCouponResponseDto(
+	int availableWishCoupon,
+	int newWishCoupon,
+	List<WishCouponDto> wishCouponList
+) {
+}
