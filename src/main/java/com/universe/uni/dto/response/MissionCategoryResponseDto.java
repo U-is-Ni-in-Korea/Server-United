cdfb7cf34e65a688a5ac2666818a246060ea9a5c
이빵주 <yungju9982@naver.com>
2023-07-13 00:05:56 +0900

    feat: Add ResponseDto

---
+++ src/main/java/com/universe/uni/dto/response/MissionCategoryResponseDto.java
@@ -0,0 +1,19 @@
+package com.universe.uni.dto.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import lombok.Builder;
+
+@JsonPropertyOrder({"id", "title", "description", "tip", "image", "missionContentList"})
+@Builder
+public record MissionCategoryResponseDto(
+	Long id,
+	String title,
+	String description,
+	String tip,
+	String image,
+	List<MissionContentResponseDto> missionContentList
+) {
+}
