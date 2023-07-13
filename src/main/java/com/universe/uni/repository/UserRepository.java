e4924df81e73f8347a94faea854bd358315079c2
이빵주 <yungju9982@naver.com>
2023-07-13 12:15:29 +0900

    delete: Delete unused function

--- src/main/java/com/universe/uni/repository/UserRepository.java
+++ src/main/java/com/universe/uni/repository/UserRepository.java
@@ -7,12 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.universe.uni.domain.entity.Couple;
 import com.universe.uni.domain.entity.User;
-import com.universe.uni.domain.entity.WishCoupon;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findBySnsAuthCode(String snsAuthCode);
 
 	List<User> findByCouple(Couple couple);
-
-	List<WishCoupon> findAllById(Long userId);
 }
