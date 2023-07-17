27aee6f3fe132ca275a70e8fdb21c0cd20282e23
이빵주 <yungju9982@naver.com>
2023-07-17 17:44:23 +0900

    feat: Add http status in ShortGameController

--- src/main/java/com/universe/uni/controller/ShortGameController.java
+++ src/main/java/com/universe/uni/controller/ShortGameController.java
@@ -2,6 +2,7 @@ package com.universe.uni.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.universe.uni.dto.request.CreateShortGameRequestDto;
@@ -27,12 +29,14 @@ public class ShortGameController {
 	private final GameService gameService;
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.OK)
 	public CreateShortGameResponseDto createShortGame(
 		@RequestBody @Valid final CreateShortGameRequestDto createShortGameRequestDto) {
 		return gameService.createShortGame(createShortGameRequestDto);
 	}
 
 	@PatchMapping("/{roundGameId}")
+	@ResponseStatus(HttpStatus.OK)
 	public GameReportResponseDto enterGameResult(
 		@PathVariable final Long roundGameId,
 		@RequestBody @Valid final EnterGameResultDto enterGameResultDto) {
@@ -40,11 +44,13 @@ public class ShortGameController {
 	}
 
 	@GetMapping("/{roundGameId}")
+	@ResponseStatus(HttpStatus.OK)
 	public GameReportResponseDto showGameReport(@PathVariable final Long roundGameId) {
 		return gameService.getGameReportIfGameIsOngoing(roundGameId);
 	}
 
 	@DeleteMapping("/{roundGameId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void quitGame(@PathVariable final Long roundGameId) {
 		gameService.quitGame(roundGameId);
 	}
