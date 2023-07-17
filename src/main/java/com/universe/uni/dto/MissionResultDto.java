928745c23f9dffa2b2d9135e0dfc9eb7a4db5419
YeongJu Lee <84129098+2zerozu@users.noreply.github.com>
2023-07-18 05:09:11 +0900

    [feat] 히스토리 response에 content 필드 추가 및 날짜/시간 포멧 수정 (#81)
    * feat: Add content in MissionResultDto
    
    * chore: Fix date and time format

--- src/main/java/com/universe/uni/dto/MissionResultDto.java
+++ src/main/java/com/universe/uni/dto/MissionResultDto.java
@@ -4,9 +4,10 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import lombok.Builder;
 
-@JsonPropertyOrder({"result", "time"})
+@JsonPropertyOrder({"content", "result", "time"})
 @Builder
 public record MissionResultDto(
+	String content,
 	String result,
 	String time
 ) {
