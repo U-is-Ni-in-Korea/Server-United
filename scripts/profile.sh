497c123d7a3ab1c33cb28d79fb719e6608d77a21
Jinsu Park <parkjinsu4755@gmail.com>
2023-07-18 07:08:22 +0900

    chore: Troubleshooting aws script quiescing (#82)
    - 스크립트 자체에서 발생하는 혼동을 줄이기 위해 파일 분리
    - appspec 으로 어떤 과정에서 에러가 나는지 확실하게 확인하기 위해 분리 진행

---
+++ scripts/profile.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=8081
+
+if [ ${CURRENT_PORT} -eq "8081" ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq "8082" ]; then
+  TARGET_PORT=8081
+else
+  TARGET_PORT=8081
+fi
+echo "${TARGET_PORT}"
---
+++ scripts/stop.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DEFAULT_PATH=/home/ubuntu/uni-sparkle-deploy/uni-sparkle
+source ${DEFAULT_PATH}/scripts/profile.sh
+
+TARGET_PORT = $(find_target_port)
+
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+if [ ! -z ${TARGET_PID} ]; then
+    echo ">> ${TARGET_PORT}에서 실행중인 서비스를 종료합니다"
+    sudo kill ${TARGET_PID}
+fi
