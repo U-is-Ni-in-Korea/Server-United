5b0d4c7291e275ec6e0bb3837c34729859626a23
Jinsu Park <parkjinsu4755@gmail.com>
2023-07-18 08:36:05 +0900

    chore: Modify Deployment Script (#83)
    * chore: Update cd script (#79)
    
    * feat: Update cd script
    
    * chore: Update cd script
    
    * chore: Fix deleted path references
    
    * chore: Fix appspec hooks
    
    * chore: Fix appspec hooks and switch script

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+DEFAULT_PATH=/home/ubuntu/uni-sparkle-deploy/uni-sparkle
 
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=8081
+
+if [ ${CURRENT_PORT} -eq "8081" ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq "8082" ]; then
+  TARGET_PORT=8081
+else
+  TARGET_PORT=8081
+fi
 echo ">> 현재 운영중인 서비스의 PORT: ${CURRENT_PORT}"
 
 echo ">> http://localhost:${CURRENT_PORT} 의 상태를 체크합니다"
@@ -12,10 +22,22 @@ for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10; do
 
   if [ ${RESPONSE_CODE} -eq 200 ]; then
     echo ">> 서비스가 성공적으로 작동 중 입니다"
-    exit 0
-  elif [ ${RETRY_COUNT} -eq 10 ]; then
+    break
+  else
+    echo ">> ${RESPONSE_CODE}"
+  fi
+  if [ ${RETRY_COUNT} -eq 10 ]; then
     echo ">> 서비스가 작동중이지 않습니다"
     exit 1
   fi
   sleep 10
 done
+
+sleep 10
+echo ">> \$service_url 에 http://127.0.0.1:${TARGET_PORT} 를 연결합니다"
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
+echo ">> 현재 Nginx proxies to ${TARGET_PORT}"
+
+sudo service nginx reload
+
+echo ">> Nginx Reload"
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,14 +4,21 @@ DEFAULT_PATH=/home/ubuntu/uni-sparkle-deploy/uni-sparkle
 JAR_NAME=$(ls ${DEFAULT_PATH}/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=${DEFAULT_PATH}/build/libs/${JAR_NAME}
 
-source ${DEFAULT_PATH}/scripts/profile.sh
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=8081
 
-TARGET_PORT = $(find_target_port)
+if [ ${CURRENT_PORT} -eq "8081" ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq "8082" ]; then
+  TARGET_PORT=8081
+else
+  TARGET_PORT=8081
+fi
 
 echo "${JAR_PATH} 를 배포합니다"
 nohup java -jar \
-        -Dspring.profiles.active=prod \
-        -Dserver.port=${TARGET_PORT} \
-        ${JAR_PATH} >/home/ubuntu/jarlog.out 2>&1 &
+  -Dspring.profiles.active=prod \
+  -Dserver.port=${TARGET_PORT} \
+  ${JAR_PATH} >/home/ubuntu/jarlog.out 2>&1 &
 echo "${TARGET_PORT} 로 새로운 서비스를 시작합니다"
 exit 0
