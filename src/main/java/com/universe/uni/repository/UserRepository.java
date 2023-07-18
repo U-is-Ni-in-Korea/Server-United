fd3fd41e76f38ecbe764b70881e5435ad688ab08
이빵주 <yungju9982@naver.com>
2023-07-18 22:03:44 +0900

    feat: Add partnerId in home response

--- src/main/java/com/universe/uni/repository/UserRepository.java
+++ src/main/java/com/universe/uni/repository/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	List<User> findByCouple(Couple couple);
 
 	User findByCoupleIdAndIdNot(Long coupleId, Long userId);
+
+	User findByCoupleId(Long coupleId);
 }
