af7184b8c23b8cd2df4de6061e0277291806f2f4
jiyeon <sjy6736@naver.com>
2023-07-20 16:52:20 +0900

    feat: Error handling when token value is null

--- src/main/java/com/universe/uni/exception/dto/ErrorType.java
+++ src/main/java/com/universe/uni/exception/dto/ErrorType.java
@@ -23,6 +23,7 @@ public enum ErrorType {
 	ALREADY_GAME_DONE(HttpStatus.BAD_REQUEST, "UE1005", "이미 종료된 라운드입니다."),
 	COUPLE_NOT_EXISTENT(HttpStatus.BAD_REQUEST, "UE1006", "존재하지 않는 커플 id 입니다"),
 	INVALID_INVITE_CODE(HttpStatus.BAD_REQUEST, "UE1007", "올바르지 않은 초대 코드입니다."),
+	TOKEN_VALUE_NOT_EXIST(HttpStatus.BAD_REQUEST, "UE1008", "토큰 값이 존재하지 않습니다."),
 
 	/**
 	 * 401 Unauthorized
