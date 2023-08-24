c2523a5ceb69ef455265d294551021b45726e1bd
jiyeon <sjy6736@naver.com>
2023-08-25 01:28:19 +0900

    feat: Add rule, example to MissionCategoryDto

--- src/main/java/com/universe/uni/dto/response/MissionCategoryDto.java
+++ src/main/java/com/universe/uni/dto/response/MissionCategoryDto.java
@@ -11,7 +11,9 @@ public class MissionCategoryDto {
 	private final Long id;
 	private final String title;
 	private final String description;
+	private final String rule;
 	private final String tip;
+	private final String example;
 	private final String image;
 	private final int level;
 	private final int expectedTime;
@@ -22,7 +24,9 @@ public class MissionCategoryDto {
 		this.id = missionCategory.getId();
 		this.title = missionCategory.getTitle();
 		this.description = missionCategory.getDescription();
+		this.rule = missionCategory.getRule();
 		this.tip = missionCategory.getTip();
+		this.example = missionCategory.getExample();
 		this.image = missionCategory.getImage();
 		this.level = missionCategory.getLevel();
 		this.expectedTime = missionCategory.getExpectedTime();
