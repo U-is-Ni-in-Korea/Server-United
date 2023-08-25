1b3c7190d4027bc9310bff67f0e5269f7138370e
jinsu4755 <parkjinsu4755@gmail.com>
2023-08-26 04:27:24 +0900

    docs(109): Add DTO document used in authentication process

--- src/main/java/com/universe/uni/exception/dto/ErrorResponse.java
+++ src/main/java/com/universe/uni/exception/dto/ErrorResponse.java
@@ -3,6 +3,7 @@ package com.universe.uni.exception.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +11,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "서버 에러 발생시 에러 바디 DTO")
 public class ErrorResponse {
 
 	@JsonProperty("code")
+    @Schema(description = "자체 에러 코드, 별도 문서 혹은 각 api 에러 코드 확인")
 	private final String uniErrorCode;
 
 	public static ErrorResponse businessErrorOf(ErrorType error) {
