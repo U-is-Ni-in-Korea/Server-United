d5ad40834f53f6167177a6595c3d12f8524a9574
jinsu4755 <parkjinsu4755@gmail.com>
2023-08-26 04:25:33 +0900

    docs(109): Add Auth Controller Docs
    인증용 Resource 에 접근하는 경우 사용되는 api 문서 정리

--- src/main/java/com/universe/uni/controller/docs/AuthControllerContract.java
+++ src/main/java/com/universe/uni/controller/docs/AuthControllerContract.java
@@ -2,10 +2,13 @@ package com.universe.uni.controller.docs;
 
 import com.universe.uni.dto.AuthTokenDto;
 import com.universe.uni.dto.request.AuthRequestDto;
+import com.universe.uni.exception.dto.ErrorResponse;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -33,10 +36,10 @@ public interface AuthControllerContract {
             ),
             @ApiResponse(
                     responseCode = "500",
-                    description = "사용자 인증의 실패한 경우 입니다.",
+                    description = "UE500: 사용자 인증에 실패한 경우 입니다.",
                     content = @Content(
                             mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(example = "{ \"code\": \"UE500\"}")
+                            schema = @Schema(implementation = ErrorResponse.class)
                     )
             )
     })
@@ -64,10 +67,10 @@ public interface AuthControllerContract {
             ),
             @ApiResponse(
                     responseCode = "500",
-                    description = "사용자 인증의 실패한 경우 입니다.",
+                    description = "UE500: 사용자 인증에 실패한 경우 입니다.",
                     content = @Content(
                             mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(example = "{ \"code\": \"UE500\"}")
+                            schema = @Schema(implementation = ErrorResponse.class)
                     )
             )
     })
@@ -79,4 +82,75 @@ public interface AuthControllerContract {
             )
             @RequestBody AuthRequestDto request
     );
+
+    @Operation(
+            summary = "Apple 인증",
+            description = "Apple 인증을 통해 유저 인증 토큰을 생성합니다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "성공",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = AuthTokenDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "UE500: 사용자 인증에 실패한 경우 입니다",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            )
+
+    })
+    @PostMapping("apple")
+    AuthTokenDto authByApple(
+            @Parameter(
+                    description = "Apple 에서 받은 identity Token",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = AuthRequestDto.class)
+                    )
+            )
+            @RequestBody AuthRequestDto request
+    );
+
+    @Operation(
+            summary = "카카오 인증 Redirect Link",
+            description = "카카오 웹 페이지 로그인을 통해 로그인한 코드를 받을 경우 사용합니다. 일반적으로 사용되지 않는 api",
+            hidden = true
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "인증 후 인증 코드를 담아서 보냅니다",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = AuthRequestDto.class)
+                    )
+            )
+    })
+    @GetMapping("kakao/callback")
+    AuthRequestDto redirectKakaoAuth(@RequestParam(name = "code") String authenticationCode);
+
+    @Operation(
+            summary = "구글 인증 Redirect Link",
+            description = "구글 웹 페이지 로그인을 통해 로그인한 코드를 받을 경우 사용합니다. 일반적으로 사용되지 않는 api",
+            hidden = true
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "인증 후 인증 코드를 담아서 보냅니다.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = AuthRequestDto.class)
+                    )
+            )
+    })
+    @GetMapping("google/callback")
+    AuthRequestDto redirectGoogleAuth(@RequestParam(name = "code") String authenticationCode);
 }
