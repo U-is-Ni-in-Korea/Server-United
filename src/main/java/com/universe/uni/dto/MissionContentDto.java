96aa4f2e2f3417fbde2eadcf19eec0b47758dabd
jinsu4755 <parkjinsu4755@gmail.com>
2023-09-04 01:38:11 +0900

    feat: fix mission content dto

--- src/main/java/com/universe/uni/dto/MissionContentDto.java
+++ src/main/java/com/universe/uni/dto/MissionContentDto.java
@@ -1,13 +1,21 @@
 package com.universe.uni.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-@JsonPropertyOrder({"id", "content"})
+@JsonPropertyOrder({"id", "content", "recommend_time"})
+@Schema(description = "미션 내용을 담는 DTO")
 @Builder
 public record MissionContentDto(
-	Long id,
-	String content
+        @Schema(description = "미션 내용 id")
+        Long id,
+        @Schema(description = "미션 내용")
+        String content,
+        @Schema(description = "미션 추천 시간")
+        @JsonProperty("recommend_time")
+        String recommendTime
 ) {
 }
