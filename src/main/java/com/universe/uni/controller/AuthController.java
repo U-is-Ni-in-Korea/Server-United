9c59c89ba0bad1271b05aee0ffeedfaa330d0dfc
Jinsu Park <parkjinsu4755@gmail.com>
2023-09-12 07:56:41 +0900

    feat: util of swagger (#128)
    * docs: fix Swagger api call
    
    api call 과정에서 cors 문제 해결
    
    * fix: Fix auth redirect api

--- src/main/java/com/universe/uni/controller/AuthController.java
+++ src/main/java/com/universe/uni/controller/AuthController.java
@@ -1,5 +1,15 @@
 package com.universe.uni.controller;
 
+import com.universe.uni.controller.docs.AuthControllerContract;
+import com.universe.uni.dto.AuthTokenDto;
+import com.universe.uni.dto.request.AuthRequestDto;
+import com.universe.uni.service.AuthServiceContract;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.universe.uni.controller.docs.AuthControllerContract;
-import com.universe.uni.dto.AuthTokenDto;
-import com.universe.uni.dto.request.AuthRequestDto;
-import com.universe.uni.service.AuthServiceContract;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -21,35 +34,39 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("auth")
 public class AuthController implements AuthControllerContract {
 
-	private final AuthServiceContract authService;
+    private final AuthServiceContract authService;
 
-	@PostMapping("kakao")
-	@Override
-	public AuthTokenDto authByKakao(@RequestBody AuthRequestDto request) {
-		return authService.authWithKakao(request.code());
-	}
-
-	@PostMapping("google")
-	@Override
-	public AuthTokenDto authByGoogle(@RequestBody AuthRequestDto request) {
-		return authService.authWithGoogle(request.code());
-	}
-
-	@PostMapping("apple")
+    @PostMapping("kakao")
     @Override
-	public AuthTokenDto authByApple(@RequestBody AuthRequestDto request) {
-		return authService.authWithAppleUser(request.code());
-	}
+    public AuthTokenDto authByKakao(@RequestBody AuthRequestDto request) {
+        return authService.authWithKakao(request.code());
+    }
 
-	@GetMapping("kakao/callback")
+    @PostMapping("google")
     @Override
-	public AuthRequestDto redirectKakaoAuth(@RequestParam(name = "code") String authenticationCode) {
-		return new AuthRequestDto(authenticationCode);
-	}
+    public AuthTokenDto authByGoogle(@RequestBody AuthRequestDto request) {
+        return authService.authWithGoogle(request.code());
+    }
 
-	@GetMapping("google/callback")
+    @PostMapping("apple")
     @Override
-	public AuthRequestDto redirectGoogleAuth(@RequestParam(name = "code") String authenticationCode) {
-		return new AuthRequestDto(authenticationCode);
-	}
+    public AuthTokenDto authByApple(@RequestBody AuthRequestDto request) {
+        return authService.authWithAppleUser(request.code());
+    }
+
+    @GetMapping("kakao/callback")
+    @Override
+    public AuthTokenDto redirectKakaoAuth(
+            @RequestParam(name = "code") String authenticationCode
+    ) {
+        return authService.authWithKakaoCode(authenticationCode);
+    }
+
+    @GetMapping("google/callback")
+    @Override
+    public AuthTokenDto redirectGoogleAuth(
+            @RequestParam(name = "code") String authenticationCode
+    ) {
+        return authService.authWithGoogleCode(authenticationCode);
+    }
 }
