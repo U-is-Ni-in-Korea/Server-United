5aaccfcb7dc5561944d6acdcd4c5b0602810d9e0
jinsu4755 <parkjinsu4755@gmail.com>
2023-09-15 03:50:03 +0900

    feat: add auth response couple id

---
+++ src/main/java/com/universe/uni/dto/response/SnsAuthResponseDto.java
@@ -0,0 +1,28 @@
+package com.universe.uni.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.universe.uni.domain.AuthToken;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "인증 토큰 및 couple 정보 DTO")
+public record SnsAuthResponseDto(
+        @JsonProperty("access_token")
+        @Schema(description = "인증 토큰")
+        String accessToken,
+        @JsonProperty("refresh_token")
+        @Schema(description = "인증 토큰이 만료 된 경우 갱신 토큰", nullable = true)
+        String refreshToken,
+        @JsonProperty("couple_id")
+        @Schema(description = "커플 연결정보 커플이 없다면 null", nullable = true)
+        Long coupleId
+) {
+    public static SnsAuthResponseDto of(AuthToken authToken, Long coupleId) {
+        return new SnsAuthResponseDto(
+                authToken.accessToken(),
+                authToken.refreshToken(),
+                coupleId
+        );
+    }
+}
--- src/main/java/com/universe/uni/service/AuthService.java
+++ src/main/java/com/universe/uni/service/AuthService.java
@@ -2,6 +2,7 @@ package com.universe.uni.service;
 
 import javax.transaction.Transactional;
 
+import com.universe.uni.dto.response.SnsAuthResponseDto;
 import com.universe.uni.external.response.GoogleAccessTokenResponse;
 import com.universe.uni.external.response.KakaoAuthResponse;
 
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 import com.universe.uni.domain.AppleTokenManager;
 import com.universe.uni.domain.SnsType;
 import com.universe.uni.domain.entity.User;
-import com.universe.uni.dto.AuthTokenDto;
+import com.universe.uni.domain.AuthToken;
 import com.universe.uni.exception.BadRequestException;
 import com.universe.uni.exception.dto.ErrorType;
 import com.universe.uni.external.response.GoogleUserInfoResponse;
@@ -33,18 +34,18 @@ public class AuthService implements AuthServiceContract {
 
     @Override
     @Transactional
-    public AuthTokenDto authWithKakaoCode(String authenticationCode) {
+    public SnsAuthResponseDto authWithKakaoCode(String authenticationCode) {
         final KakaoAuthResponse kakaoAuth = kakaoRepository.fetchTokenBy(authenticationCode);
         return authWithKakao(kakaoAuth.accessToken());
     }
 
 	@Override
 	@Transactional
-	public AuthTokenDto authWithKakao(String accessToken) {
+	public SnsAuthResponseDto authWithKakao(String accessToken) {
 		final KakaoUserResponse kakaoUser = kakaoRepository.getUser(accessToken);
 		final User user = userRepository.findBySnsAuthCode(kakaoUser.id().toString())
 			.orElseGet(() -> userRepository.save(registerKakaoUser(kakaoUser)));
-		return beIssuedAuthToken(user.getId());
+		return beIssuedAuthToken(user);
 	}
 
 	private User registerKakaoUser(KakaoUserResponse kakaoUser) {
@@ -56,18 +57,18 @@ public class AuthService implements AuthServiceContract {
 
     @Override
     @Transactional
-    public AuthTokenDto authWithGoogleCode(String authenticationCode) {
+    public SnsAuthResponseDto authWithGoogleCode(String authenticationCode) {
         final GoogleAccessTokenResponse googleAuth = googleRepository.fetchTokenBy(authenticationCode);
         return authWithGoogle(googleAuth.idToken());
     }
 
 	@Override
 	@Transactional
-	public AuthTokenDto authWithGoogle(String accessToken) {
+	public SnsAuthResponseDto authWithGoogle(String accessToken) {
 		final GoogleUserInfoResponse googleUser = googleRepository.getUser(accessToken);
 		final User user = userRepository.findBySnsAuthCode(googleUser.mSub())
 			.orElseGet(() -> userRepository.save(registerGoogleUser(googleUser)));
-		return beIssuedAuthToken(user.getId());
+		return beIssuedAuthToken(user);
 	}
 
 	private User registerGoogleUser(GoogleUserInfoResponse googleUser) {
@@ -79,11 +80,11 @@ public class AuthService implements AuthServiceContract {
 
 	@Override
 	@Transactional
-	public AuthTokenDto authWithAppleUser(String identityToken) {
+	public SnsAuthResponseDto authWithAppleUser(String identityToken) {
 		final String userEmail = appleTokenManager.decodeEmail(identityToken);
 		final User user = userRepository.findBySnsAuthCode(userEmail)
 			.orElseGet(() -> userRepository.save(registerAppleUser(userEmail)));
-		return beIssuedAuthToken(user.getId());
+		return beIssuedAuthToken(user);
 	}
 
 	private User registerAppleUser(String email) {
@@ -93,8 +94,9 @@ public class AuthService implements AuthServiceContract {
 			.build();
 	}
 
-	private AuthTokenDto beIssuedAuthToken(long userId) {
-		return jwtManager.issueToken(userId);
+	private SnsAuthResponseDto beIssuedAuthToken(User user) {
+        final AuthToken authToken = jwtManager.issueToken(user.getId());
+        return SnsAuthResponseDto.of(authToken, user.getCouple().getId());
 	}
 
 	@Override
