1cf3a3090ce86fe1b101fc053c2ed1500af889e5
jinsu4755 <parkjinsu4755@gmail.com>
2023-09-16 02:39:31 +0900

    docs: Add CoupleDto Schema docs

--- src/main/java/com/universe/uni/dto/response/CoupleDto.java
+++ src/main/java/com/universe/uni/dto/response/CoupleDto.java
@@ -1,18 +1,25 @@
 package com.universe.uni.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
  * DTO for {@link com.universe.uni.domain.entity.Couple}
  */
+@Schema(description = "커플 연결 결과 DTO")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record CoupleDto(
-	Long id,
-	LocalDate startDate,
-	String inviteCode,
-	int heartToken
+        @Schema(description = "커플 아이디")
+        Long id,
+        @Schema(description = "커플 디데이")
+        LocalDate startDate,
+        @Schema(description = "커플 초대 코드")
+        String inviteCode,
+        @Schema(description = "커플의 하트 코인 잔여량")
+        int heartToken
 ) implements Serializable {
 }
