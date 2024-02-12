6672e26758be721820048d816b3010e3cf16bf54
jinsu4755 <parkjinsu4755@gmail.com>
2024-02-12 15:55:59 +0900

    fix: Fix User Table name to Users

--- src/main/java/com/universe/uni/domain/entity/User.java
+++ src/main/java/com/universe/uni/domain/entity/User.java
@@ -20,7 +20,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
