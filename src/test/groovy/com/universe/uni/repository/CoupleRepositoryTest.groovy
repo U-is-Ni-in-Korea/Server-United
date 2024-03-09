4ebf79b761880f2413cc8abb5cf15ba1de8f6f5f
jinsu4755 <parkjinsu4755@gmail.com>
2024-03-09 17:31:04 +0900

    fix: fix save test

--- src/test/groovy/com/universe/uni/repository/CoupleRepositoryTest.groovy
+++ src/test/groovy/com/universe/uni/repository/CoupleRepositoryTest.groovy
@@ -1,19 +1,24 @@
 package com.universe.uni.repository
 
+import com.universe.uni.domain.InviteCodeStrategy
 import com.universe.uni.domain.entity.Couple
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.dao.OptimisticLockingFailureException
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.support.TransactionTemplate
 import spock.lang.Specification
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CoupleRepositoryTest extends Specification {
 
     @Autowired
@@ -21,6 +26,52 @@ class CoupleRepositoryTest extends Specification {
 
     @Autowired
     PlatformTransactionManager transactionManager
+
+    def "커플 아이디를 바탕으로 커플을 조회할 수 있다"() {
+        given:
+        Long coupleId = 1L
+        when:
+        Couple couple = coupleRepository.findById(coupleId).get()
+        then:
+        couple.id == coupleId
+        noExceptionThrown()
+    }
+
+    def "커플은 기념일을 바탕으로 생성되어야 한다"() {
+        setup:
+        InviteCodeStrategy inviteCodeStrategy = Mock()
+        and:
+        LocalDate date = LocalDate.parse("1010-11-11", DateTimeFormatter.ISO_LOCAL_DATE)
+
+        when:
+        Couple couple = Couple.builder()
+            .startDate(date)
+            .inviteCode(inviteCodeStrategy.generate())
+            .build()
+        and:
+        coupleRepository.saveAndFlush(couple)
+
+        then:
+        inviteCodeStrategy.generate() >> "testCode0"
+        and:
+        coupleRepository.findByInviteCode("testCode0").get().id == 1002L
+
+    }
+
+    def "커플의 기념일 정보를 수정할 수 있다"() {
+        given:
+        Long coupleId = 1L
+        and:
+        LocalDate date = LocalDate.parse("0001-11-11", DateTimeFormatter.ISO_LOCAL_DATE)
+
+        when:
+        Couple couple = coupleRepository.findById(coupleId).get()
+        couple.updateStartDate(date)
+        Couple savedCouple = coupleRepository.saveAndFlush(couple)
+
+        then:
+        savedCouple.getStartDate() == date
+    }
 
     def "커플 초대 코드를 바탕으로 초대코드를 가진 커플을 조회할 수 있다"() {
         given: "커플 초대 코드가 주어지면"
@@ -42,14 +93,14 @@ class CoupleRepositoryTest extends Specification {
             template.execute(status -> {
                 Couple couple = coupleRepository.findWithOptimisticForceIncrementById(coupleId).get()
                 couple.decreaseHeartToken()
-                coupleRepository.save(couple)
+                coupleRepository.saveAndFlush(couple)
             })
         }
         Future updateCase2 = executor.submit {
             template.execute(status -> {
                 Couple couple = coupleRepository.findWithOptimisticForceIncrementById(coupleId).get()
                 couple.decreaseHeartToken()
-                coupleRepository.save(couple)
+                coupleRepository.saveAndFlush(couple)
             })
         }
 
--- src/test/groovy/com/universe/uni/repository/UserRepositoryTest.groovy
+++ src/test/groovy/com/universe/uni/repository/UserRepositoryTest.groovy
@@ -6,7 +6,6 @@ import com.universe.uni.domain.entity.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import spock.lang.Specification
-import spock.lang.Unroll
 
 @DataJpaTest
 class UserRepositoryTest extends Specification {
@@ -14,19 +13,15 @@ class UserRepositoryTest extends Specification {
     @Autowired
     private UserRepository userRepository
 
-    @Unroll
     def "유저는 SNS 정보를 사용하여 생성할 수 있다"() {
-        given: "유저의 SNS 정보가 주어지면"
-        String expectedSnsAuthCode = "SNS_AUTH_CODE"
-
-        and: "유저 Entity 를 생성하고"
+        given: "유저 Entity 를 생성하고"
         User user = User.builder()
                 .snsType(expectedSnsType)
                 .snsAuthCode(expectedSnsAuthCode)
                 .build()
 
         when: "유저를 저장하면"
-        User actualUser = userRepository.save(user)
+        User actualUser = userRepository.saveAndFlush(user)
 
         then: "유저의 정보가 저장된다"
         actualUser.id != null
@@ -34,24 +29,27 @@ class UserRepositoryTest extends Specification {
         actualUser.snsAuthCode == expectedSnsAuthCode
 
         where:
-        expectedSnsType | _
-        SnsType.KAKAO   | _
-        SnsType.GOOGLE  | _
-        SnsType.APPLE   | _
+        expectedSnsType | expectedSnsAuthCode
+        SnsType.KAKAO   | "test1"
+        SnsType.GOOGLE  | "test2"
+        SnsType.APPLE   | "test3"
     }
 
     def "유저 닉네임을 변경할 수 있다"() {
-        given: "새롭게 변경할 닉네임이 주어지고"
+        setup:
+        def target = 1001L
+        and: "새롭게 변경할 닉네임이 주어지고"
         String expectedNickname = "test"
 
         and: "변경할 유저를 조회하여"
-        User user = userRepository.findById(1L).get()
+        User user = userRepository.findById(target).get()
 
         when: "이름을 바꾸면"
         user.updateNickname(expectedNickname)
+        userRepository.saveAndFlush(user)
 
         then: "DB에 유저의 이름이 변경되어 저장된다"
-        userRepository.findById(1L).get().nickname == expectedNickname
+        userRepository.findById(target).get().nickname == expectedNickname
         noExceptionThrown()
     }
 
@@ -60,7 +58,7 @@ class UserRepositoryTest extends Specification {
         String nickname = "12345678901"
 
         and: "변경할 유저를 조회하여"
-        User user = userRepository.findById(1L).get()
+        User user = userRepository.findById(1001L).get()
 
         when: "유저 이름을 변경하면"
         user.updateNickname(nickname)
@@ -70,7 +68,7 @@ class UserRepositoryTest extends Specification {
         exception.message == "nickname exceeds the maximum length of 10 characters"
 
         and: "유저 이름은 변경되지 않는다."
-        userRepository.findById(1L).get().nickname != nickname
+        userRepository.findById(1001L).get().nickname != nickname
     }
 
     def "이미 가입된 유저의 SNS 와 인증번호로 유저를 검색할 수 있다"() {
@@ -79,12 +77,12 @@ class UserRepositoryTest extends Specification {
 
         expect: "유저를 찾을 수 있다"
         User user = userRepository.findBySnsAuthCode(snsAuthCode).get()
-        user.id == 1L
+        user.id == 1001
     }
 
     def "커플 엔티티를 바탕으로 커플로 연결된 유저를 찾을 수 있다"() {
         given: "커플 정보가 주어지면"
-        Couple couple = userRepository.findById(1L).get().couple
+        Couple couple = userRepository.findById(1001L).get().couple
 
         expect: "커플에 연결된 유저들을 조회할 수 있다"
         List<User> users = userRepository.findByCouple(couple)
@@ -94,17 +92,17 @@ class UserRepositoryTest extends Specification {
 
     def "자신의 유저 id 와 커플 id를 바탕으로 커플로 연결된 상대 유저를 조회할 수 있다"() {
         given: "자신의 유저 id 와 커플 id 가 주어지면"
-        Long userId = 1L
-        Long coupleId = 1L
+        Long userId = 1001L
+        Long coupleId = 1001L
 
         expect: "커플로 연결된 상대 유저를 조회할 수 있다"
         User user = userRepository.findByCoupleIdAndIdNot(coupleId, userId)
-        user.id == 2L
+        user.id == 1002L
     }
 
     def "커플 아이디를 바탕으로 커플에 연결된 유저 수를 조회할 수 있다"() {
         given: "커플 아이디가 주어지면"
-        Long coupleId = 1L
+        Long coupleId = 1001L
 
         expect: "커플에 연결된 유저 수를 조회할 수 있다"
         userRepository.countByCoupleId(coupleId) == 2
